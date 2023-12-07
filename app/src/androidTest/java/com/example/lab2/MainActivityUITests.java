996de0b11d55e0f700b7faafd451de7a623a1929
N4gl1ux <nagliuxpro@gmail.com>
2023-12-07 20:33:27 +0200

    Added UI and Unit tests

---
+++ app/src/androidTest/java/com/example/lab2/MainActivityUITests.java
@@ -0,0 +1,152 @@
+package com.example.lab2;
+
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityUITests {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityUITests() {
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.edUserInput),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("This is a test!"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.edUserInput), withText("This is a test!"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText2.perform(pressImeActionButton());
+
+        ViewInteraction appCompatSpinner = onView(
+                allOf(withId(R.id.spSelectionOptions),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatSpinner.perform(click());
+
+        DataInteraction appCompatCheckedTextView = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(0);
+        appCompatCheckedTextView.perform(click());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.btnCount), withText("Count"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.tvMain), withText("4"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        textView.check(matches(withText("4")));
+
+        ViewInteraction appCompatSpinner2 = onView(
+                allOf(withId(R.id.spSelectionOptions),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatSpinner2.perform(click());
+
+        DataInteraction appCompatCheckedTextView2 = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(1);
+        appCompatCheckedTextView2.perform(click());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.btnCount), withText("Count"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.tvMain), withText("15"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        textView2.check(matches(withText("15")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
--- app/src/test/java/com/example/lab2/ExampleUnitTest.java
+++ app/src/test/java/com/example/lab2/ExampleUnitTest.java
@@ -14,4 +14,59 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void testCountCharacters() {
+        final String givenString = ("This is a test!");
+        final int expectedResult = 15;
+
+        final int actualResult = TextCounter.countCharacters(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void testCountCharactersGivenEmpty() {
+        final String givenString = ("");
+        final int expectedResult = 0;
+
+        final int actualResult = TextCounter.countCharacters(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void testCountWords() {
+        final String givenString = "This is a test!";
+        final int expectedResult = 4;
+
+        final int actualResult = TextCounter.countWords(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void testCountWordsGivenWhitespace() {
+        final String givenString = "This     is  a      test                !";
+        final int expectedResult = 4;
+
+        final int actualResult = TextCounter.countWords(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void testCountWordsGivenRandomSymbols() {
+        final String givenString = "This is a  * >><.,,,,;----++ test!";
+        final int expectedResult = 4;
+
+        final int actualResult = TextCounter.countWords(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void testCountWordsGivenEmpty() {
+        final String givenString = "";
+        final int expectedResult = 0;
+
+        final int actualResult = TextCounter.countWords(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
 }
